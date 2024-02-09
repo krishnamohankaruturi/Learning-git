e27b6aac3165e39e2140c97d2424e44ef5daa38e
krishnamohankaruturi <45046233+krishnamohankaruturi@users.noreply.github.com>
2024-02-09 21:34:47 +0530

    Feature/feature785 (#5)
    * pom.xml Commit
    
    * krishna mohan Commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,6 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "I am Learning. This is another User and more changes" );
+
+        System.out.println( "I am Learning. This is another User and more changes" and krishna mohan );
+
         System.out.println( "I am Learning. This is another User and more changes" and anothee );
+
     }
 }
